7a756a8af9cdc8309624e8d9aa6ebb5e4afa0601
Matias Rasmussen <rasmussen.matias@gmail.com>
2015-02-04 14:29:36 +0100

    Added gitignore and parser file stub

---
+++ PubMedParser/parser.py
@@ -0,0 +1,6 @@
+__author__ = 'matias'
+
+import xml.etree.ElementTree as ET
+
+
+print ET.parse(r"C:\Users\matias\Desktop\thesis\data\articles.A-B\Am_J_Case_Rep\Am_J_Case_Rep_2012_Aug_14_13_180-182.nxml")
