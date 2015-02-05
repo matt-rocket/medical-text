671d4937c38c4cdece943c5a7fd3a59fe787d30b
Matias Rasmussen <rasmussen.matias@gmail.com>
2015-02-05 09:29:33 +0100

    Extract and save case-report articles in .txt

--- PubMedParser/parser.py
+++ PubMedParser/parser.py
@@ -1,6 +1,36 @@
 __author__ = 'matias'
 
+import os
 import xml.etree.ElementTree as ET
 
 
-print ET.parse(r"C:\Users\matias\Desktop\thesis\data\articles.A-B\Am_J_Case_Rep\Am_J_Case_Rep_2012_Aug_14_13_180-182.nxml")
+def get_type(filepath):
+    tree = ET.parse(filepath)
+    root = tree.getroot()
+    return root.attrib['article-type']
+
+
+root_folder = r"C:\Users\matias\Desktop\thesis\data\pubmed\articles.C-H"
+
+types = {}
+hits = 0
+total = 0
+
+case_reports = []
+
+for subdir, dirs, files in os.walk(root_folder):
+    for filename in files:
+        total += 1
+        filepath = os.path.join(subdir, filename)
+        article_type = get_type(filepath)
+        if article_type == "case-report":
+            print filepath
+            case_reports.append(filepath)
+            hits += 1
+        else:
+            print hits,"/",total
+
+with open('case_report_list_C-H.txt','w') as outfile:
+    for filename in case_reports:
+        outfile.write("%s\n" % filename)
+
