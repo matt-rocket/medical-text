480014b68195ac3041435236152507c558002630
Matias Rasmussen <rasmussen.matias@gmail.com>
2015-02-09 11:25:38 +0100

    Added caching of trie structure. Minor bugfix in DiseaseExteactor
    - fixed end-of-tokenstream bug

--- PubMedParser/entityextractor.py
+++ PubMedParser/entityextractor.py
@@ -2,12 +2,24 @@ __author__ = 'matias'
 
 import xml.etree.ElementTree as ET
 from nltk import word_tokenize
-
+import os.path
+import pickle
 
 
 class TokenTrie(object):
     def __init__(self):
         self.path = {}
+        # try to load from cache
+        if os.path.isfile("trie.cache"):
+            try:
+                print "loading trie cache.."
+                self.path = pickle.load(open("trie.cache"))
+            except:
+                raise IOError("not good!")
+            print "succesfully loaded trie cache.."
+
+    def is_empty(self):
+        return self.path == {}
 
     def add(self,seq):
         head = self.path
@@ -18,6 +30,10 @@ class TokenTrie(object):
                 head[token] = {}
                 head = head[token]
         head[True] = True
+
+    def save_to_cache(self):
+        with open("trie.cache",'w') as outfile:
+            pickle.dump(self.path, outfile)
 
     def scan(self,seq):
         found_seqs = []
@@ -31,7 +47,8 @@ class TokenTrie(object):
                 if True in head and match:
                     # pass list by value
                     found_seqs.append(match[:])
-                while seq and seq[step] in head:
+                while len(seq)>step and seq[step] in head:
+                    print seq[step]
                     match.append(seq[step])
                     head = head[seq[step]]
                     step += 1
@@ -45,22 +62,24 @@ class TokenTrie(object):
 class DiseaseExtractor(object):
     def __init__(self):
         self.trie = TokenTrie()
-        disease_names = open('../../data/UMLS/diseases.txt').read().split('\n')
-        count = 0
-        for disease in disease_names:
-            self.trie.add(word_tokenize(disease))
-            print disease
-            count += 1
-            #if count > 100000:
-            #    break
+        if self.trie.is_empty():
+            disease_names = open('../../data/UMLS/diseases.txt').read().split('\n')
+            count = 0
+            for disease in disease_names:
+                self.trie.add(word_tokenize(disease))
+                print disease
+                count += 1
+            self.trie.save_to_cache()
 
     def extract(self, text):
         return self.trie.scan(word_tokenize(text))
 
 class CaseReports(object):
-    def __init__(self,section='A-B'):
+    def __init__(self,section='A-B', filename=None):
         with open("case_report_list_%s.txt" % section) as infile:
             self.filenames = infile.read().split("\n")
+        if filename:
+            self.filenames = [filename]
 
     def __iter__(self):
         for filename in self.filenames:
@@ -68,20 +87,23 @@ class CaseReports(object):
             root = tree.getroot()
             title = root.find('./front/article-meta/title-group/article-title').text
             body_node = root.find('./body')
-            body = ET.tostring(body_node,encoding='utf8',method='text').replace("\n", " ")
-            yield body
+            if body_node is not None:
+                body = ET.tostring(body_node,encoding='utf8',method='text').replace("\n", " ")
+                yield body
+            else:
+                continue
 
 
 
 
-ab_cases = CaseReports('A-B')
+ab_cases = CaseReports('A-B', r"C:\Users\matias\Desktop\thesis\data\pubmed\articles.A-B\Ann_Gastroenterol\Ann_Gastroenterol_2013_26(2)_165.nxml")
 d_extractor = DiseaseExtractor()
 
-print d_extractor.extract("I have a 11q syndrome in my head. I'm so 11q and zws right now.")
 
 
-for case in ab_cases:
-    print d_extractor.extract(case)
+for body in ab_cases:
+    print body
+    print d_extractor.extract(body)
 
 
 """
