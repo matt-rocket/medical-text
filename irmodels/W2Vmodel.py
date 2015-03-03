edf8ef26537b2410fae70bc74ed8f80a74d9d047
Matias Rasmussen <rasmussen.matias@gmail.com>
2015-03-03 22:32:29 +0100

    W2Vmodel implementation

--- irmodels/W2Vmodel.py
+++ irmodels/W2Vmodel.py
@@ -1,18 +1,37 @@
 __author__ = 'matias'
 
-from textanalysis.texts import SentenceStream
+from textanalysis.texts import RawSentenceStream
+from textanalysis.phrasedetection import PmiPhraseDetector
+from gensim.models.word2vec import Word2Vec
 import logging
-from gensim.models.phrases import Phrases
+import os
 
 
-# setup logging
-logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+class W2Vmodel(object):
+    def __init__(self):
+        self.model = None
 
-sentences = SentenceStream()
+        # parameters
+        self.dataset = "CASEREPORT"
+        self.sentences = RawSentenceStream()
+        self.detector = PmiPhraseDetector()
 
-bigram = Phrases.learn_vocab(sentences,max_vocab_size=4000000)
+        # data file path
+        models_folder = os.path.join(*[os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'data', 'models'])
+        filename = "WORD2VEC_%s" % (self.dataset, )
+        self.filepath = os.path.join(models_folder, filename)
+        model_exists = os.path.isfile(self.filepath)
+
+        if model_exists:
+            logging.info("found data file %s" % (self.filepath, ))
+            self.model = Word2Vec.load(self.filepath)
+        else:
+            self.model = Word2Vec(sentences=self.sentences)
+            self.model.save(self.filepath)
 
 
-
-for word in bigram[1]:
-    print word
+if __name__ == "__main__":
+    # setup logging
+    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+    # build model
+    m = W2Vmodel()
