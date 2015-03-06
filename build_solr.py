3885bb11eb62a1c7ad497cc389232f83117ad721
Matias Rasmussen <rasmussen.matias@gmail.com>
2015-03-06 13:22:02 +0100

    Now indexing FindZebra Articles on Solr

--- build_solr.py
+++ build_solr.py
@@ -1,27 +1,26 @@
 __author__ = 'matias'
 
-from textanalysis.entityextractor import DiseaseExtractor
-from textanalysis.texts import CaseReportLibrary
 import solr
+import os
+import json
 
 solr_con = solr.SolrConnection('http://localhost:8983/solr')
 
-d_extractor = DiseaseExtractor()
-
-case_reports = CaseReportLibrary()
-
 count = 0
-hits = 0
 
-for case_report in case_reports:
-    count += 1
-    print "added",count
-    solr_con.add(_commit=False, id=count,
-                 title=case_report.title.decode('utf-8'),
-                 description=case_report.abstract.decode('utf-8'),
-                 text=case_report.body.decode('utf-8'),
-                 keywords=",".join(case_report.mesh_terms).decode('utf-8'),
-                 resourcename=case_report.filename,
-                 )
+for subdir, dirs, files in os.walk("data/articles/findzebra"):
+    for filename in files:
+        with open("data/articles/findzebra/%s" % (filename, ), 'r') as infile:
+            data = json.load(infile)
+            title = data[u'title'].encode('utf-8')
+            text = data[u'content'].encode('utf-8')
+            count += 1
+            print "added",count
+
+            solr_con.add(_commit=False, id=count,
+                         title=title.decode('utf-8'),
+                         text=text.decode('utf-8'),
+                         resourcename=filename.decode('utf-8'),
+                         )
 
 solr_con.commit()
