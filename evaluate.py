de2165dc1b3fda1adf905e1bb157dd15e4de5ef5
Matias Rasmussen <rasmussen.matias@gmail.com>
2015-03-10 16:18:19 +0100

    Added TermwiseW2VExpansion

--- evaluate.py
+++ evaluate.py
@@ -1,7 +1,7 @@
 __author__ = 'matias'
 
 from search import StandardSolrEngine
-from queryexpansion import AverageW2VExpansion
+from queryexpansion import AverageW2VExpansion, TermwiseW2VExpansion
 from evaluation.metrics import *
 import logging
 
@@ -12,11 +12,16 @@ def evaluate(search_engine, k, verbose=False):
 
         query_results = []
 
+        recalls = []
+        precisions = []
+
         for record in records:
             qid, query, answer, relevant, partly_relevant = record.split("\t")
             correct_answers = [ans for ans in relevant.split(",") + partly_relevant.split(",") if ans is not '']
             response = search_engine.query(query, top_n=k)
             returned_docids = [hit[u'id'] for hit in response.results]
+            recalls.append(recall(correct_answers, returned_docids))
+            precisions.append(precision(correct_answers, returned_docids))
             if verbose:
                 print query
                 print "P@%s" % (k,), precision(correct_answers,returned_docids)
@@ -27,17 +32,20 @@ def evaluate(search_engine, k, verbose=False):
         print str(search_engine)
         print "MAP", mean_average_precision(query_results)
         print "MRR", mean_reciprocal_rank(query_results)
-
+        print "Avg. R@k", sum(recalls)/float(len(recalls))
+        print "Avg. P@k", sum(precisions)/float(len(precisions))
+        print recalls
 
 if __name__ == "__main__":
     # setup logging
-    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+    # logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
     # retrieval top k ranked
     k = 20
     search_engines = [
         StandardSolrEngine(),
         StandardSolrEngine(query_expansion=AverageW2VExpansion()),
+        StandardSolrEngine(query_expansion=TermwiseW2VExpansion()),
         ]
 
     for engine in search_engines:
-        evaluate(engine, k)
+        evaluate(engine, k, verbose=False)
--- queryexpansion.py
+++ queryexpansion.py
@@ -17,22 +17,48 @@ class QueryExpansion(object):
 class AverageW2VExpansion(QueryExpansion):
 
     def __init__(self):
-        self.name = "W2V Query Expansion"
         # phrase detector
         self.phrase_detector = PmiPhraseDetector(RawSentenceStream())
         # number converter
         self.tokenizer = RawTokenizer()
         # build model
         self.model = W2Vmodel(PhraseSentenceStream(self.phrase_detector))
+        # parameters
+        self.p = 0.7
+        self.n = 10
 
     def expand(self, query):
-        # parameters
-        threshold = 0.7
-        n = 10
         phrases = self.phrase_detector.detect(self.tokenizer.tokenize(query.lower()))
         w2v_phrases = [phrase for phrase in phrases if phrase in self.model]
-        similar_phrases = self.model.inner_model.most_similar(w2v_phrases, [], topn=n)
-        extra_terms = " ".join([phrase[0].replace('_', ' ') for phrase in similar_phrases if phrase[1] > threshold])
+        similar_phrases = self.model.inner_model.most_similar(w2v_phrases, [], topn=self.n)
+        extra_terms = " ".join([phrase[0].replace('_', ' ') for phrase in similar_phrases if phrase[1] > self.p])
+        return "%s %s" % (query, extra_terms, )
+
+    def __str__(self):
+        return self.__class__.__name__
+
+
+class TermwiseW2VExpansion(QueryExpansion):
+
+    def __init__(self):
+        # phrase detector
+        self.phrase_detector = PmiPhraseDetector(RawSentenceStream())
+        # number converter
+        self.tokenizer = RawTokenizer()
+        # build model
+        self.model = W2Vmodel(PhraseSentenceStream(self.phrase_detector))
+        # parameters
+        self.p = 0.7
+        self.n = 10
+
+    def expand(self, query):
+        phrases = self.phrase_detector.detect(self.tokenizer.tokenize(query.lower()))
+        w2v_phrases = [phrase for phrase in phrases if phrase in self.model]
+        translated_phrases = []
+        for term in w2v_phrases:
+            closest_term = self.model.inner_model.most_similar([term], [], topn=self.n)[0][0]
+            translated_phrases.append(closest_term)
+        extra_terms = " ".join(translated_phrases)
         return "%s %s" % (query, extra_terms, )
 
     def __str__(self):
