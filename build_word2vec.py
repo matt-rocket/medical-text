be3845613a78a9898e0d611c19ce808f43b496ad
Matias Rasmussen <rasmussen.matias@gmail.com>
2015-03-13 22:36:28 +0100

    clean up script

--- build_word2vec.py
+++ build_word2vec.py
@@ -13,13 +13,7 @@ phrase_detector = PmiPhraseDetector(RawSentenceStream())
 # build model
 m = W2Vmodel(PhraseSentenceStream(phrase_detector))
 
-print (u'arrhythmias' in m)
-print (u'heart' in m)
-print (u'megameatus' in m)
 
-
-
-"""
 print m.inner_model.most_similar(u"colon_cancer".split())
 print m.inner_model.most_similar(u"new_york michigan california".split())
 print m.inner_model.most_similar(u"pain back back_pain lumbar".split())
@@ -38,26 +32,4 @@ print m.inner_model.most_similar(u"bleeding colon burning rectum disease".split(
 print m.inner_model.most_similar(u"bacteria".split())
 print m.inner_model.most_similar(u"christian muslim".split())
 print m.inner_model.most_similar(u"influenza cancer copd asthma colitis blind deaf gout diabetes".split())
-"""
 
-
-
-"""
-query = u"lowset ears".split()
-
-tokens = phrase_detector.detect(query)
-combined_sim = m.inner_model.most_similar(tokens, topn=50)
-token_sims = [m.inner_model.most_similar(token, topn=50) for token in tokens]
-
-print "COMBINED:"
-print combined_sim
-
-intersection = set([e[0] for e in combined_sim])
-
-print "TOKENS:"
-for sim in token_sims:
-    print sim
-    intersection = intersection.intersection(set([e[0] for e in sim]))
-
-print list(intersection)
-"""
